cc51b019879ec6a287e88f83c1a40493d5da25ee
Marat Nagayev <nagaevmt49@gmail.com>
2021-04-12 22:17:24 +0300

    Update prog3.cpp

--- prog3.cpp
+++ prog3.cpp
@@ -1,57 +1,69 @@
-/******************************************************************************
-
-                              Online C++ Compiler.
-               Code, Compile, Run and Debug C++ program online.
-Write your code in this editor and press "Run" button to compile and execute it.
-
-*******************************************************************************/
-
 #include <iostream>
+#include <string>
 #include <vector>
-#include <string.h>
+
 using namespace std;
 
 int main()
 {
-    int start,end,symbols;
-    vector <int> v;
-    char s[25];
-    start = 0;
-    end = 0;
-    symbols = 0;
-    cout<<"Введите текст: ";
-    fgets(s,sizeof(s),stdin);
-    int curr = 0;
+    vector<string> v;
+    string s, tmp;
+    getline(cin, s);
+    int words = 0;
     float max = 0.0f;
-    for(int i=0;i<strlen(s);i++){
-        char c = s[i];
-        if (c=='a' || c=='e' || c=='i' || c=='o' || c=='u' || c=='y'){
-            curr++;
+    int symbols, all_symbols;
+    symbols = 0;
+    all_symbols = 0;
+    for (int i = 0; i < s.size(); i++)
+    {
+        if (isspace(s[i]))
+        {
+            v.push_back(tmp);
+            tmp = "";
         }
-        if(isspace(s[i])){
-            symbols--;
-            if (curr>=max && curr!=0){
-                v.push_back(start);
-                v.push_back(end);
-            }
-        
-            curr = 0;
-            start = end+1;
+        else
+        {
+            tmp.push_back(s[i]);
         }
-        symbols++;
-        end++;
     }
-    if (v.size()==0){
-        cout<<"Некорректные данные"<<endl;
-    } else {
-        cout<<"Слово (слова): ";
-        for(int i=0;i<v.size();i++){
-            for(int j=v[i];j<v[i+1];j++){
-                cout<<s[j];
-            }
-            cout<<" ";
-        }
-        cout<<endl;
+    if (tmp != "")
+    {
+        v.push_back(tmp);
     }
+
+    //Now we have vector with words
+    //find max
+    for (int i = 0; i < v.size(); i++)
+    {
+        all_symbols = v[i].size();
+        symbols = 0;
+        for (int j = 0; j < all_symbols; j++)
+        {
+            char c = tolower(v[i][j]);
+            if (c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u' || c == 'y')
+            {
+                symbols++;
+            }
+        }
+        if (symbols / all_symbols > max)
+            max = symbols / all_symbols;
+    }
+    //find words max
+    for (int i = 0; i < v.size(); i++)
+    {
+        all_symbols = v[i].size();
+        symbols = 0;
+        for (int j = 0; j < v[i].size(); j++)
+        {
+            char c = tolower(v[i][j]);
+            if (c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u' || c == 'y')
+            {
+                symbols++;
+            }
+        }
+        if (symbols / all_symbols == max)
+            cout << v[i];
+    }
+    cout << endl;
     return 0;
 }
