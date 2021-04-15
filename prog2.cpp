9babdb548f65c6852e34e2f20bbbccf4b01b90c4
Marat Nagayev <nagaevmt49@gmail.com>
2021-04-15 15:21:12 +0300

    Update prog2.cpp

--- prog2.cpp
+++ prog2.cpp
@@ -1,16 +1,29 @@
+#include "stdafx.h"
 #include <iostream>
 #include <string>
 #include <time.h>
+#include <conio.h>
+#include <Windows.h>
 
 using namespace std;
 
 int main()
 {
-    bool running = true;
+	SetConsoleOutputCP(1251);
+    SetConsoleCP(1251);
     string n_s,a_s,b_s;
     int a,b,n;
     srand(time(0));
-    while(running){
+	cout<<"Введите размер матрицы: ";
+    getline(cin, n_s);
+    n = atoi(n_s.c_str());
+    cout<<"Введите нижнюю границу: ";
+    getline(cin, a_s);
+    a = atoi(a_s.c_str());
+    cout<<"Введите верхнюю границу: ";
+    getline(cin, b_s);
+    b = atoi(b_s.c_str());
+    while(!((n!=0 && n_s[0]!='0') || (a!=0 && a_s[0]!='0') || (b!=0 && b_s[0]!=0))){
        cout<<"Введите размер матрицы: ";
        getline(cin, n_s);
        n = atoi(n_s.c_str());
@@ -20,15 +33,14 @@ int main()
        cout<<"Введите верхнюю границу: ";
        getline(cin, b_s);
        b = atoi(b_s.c_str());
-       //correct
-       if ( !(n!=0 && n_s[0]!='0') || (a!=0 && a_s[0]!='0') || (b!=0 && b_s[0]!=0) ) break;
     }
-    int arr[n][n];
+    int **arr=new int*[n];
+	for(int i=0;i<n;i++) arr[i]=new int[n];
     cout<<"Матрица: "<<endl;
     for(int i=0;i<n;i++){
         for(int j=0;j<n;j++){
             if (i+j==n-1) arr[i][j]=0;
-            else arr[i][j]=random()%(a-b+1)+a;
+            else arr[i][j]=rand()%(a-b+1)+a;
             cout<<arr[i][j]<<" ";
         }
         cout<<endl;
@@ -41,4 +53,5 @@ int main()
         for(int j=0;j<n;j++) cout<<arr[i][j]<<" ";
         cout<<endl;
     }
+	_getch();
 }
