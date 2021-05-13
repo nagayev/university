5ee3b089db206513c1404f3d51be4410a8a02139
Marat Nagayev <nagaevmt49@gmail.com>
2021-05-13 11:59:16 +0300

    Update prog4.cpp

--- prog4.cpp
+++ prog4.cpp
@@ -1,51 +1,70 @@
-// Nagayev4.cpp: определяет точку входа для консольного приложения.
+// Nagayev5.cpp: определяет точку входа для консольного приложения.
 //
 
 #include "stdafx.h"
-#include <iostream>
 #include <conio.h>
 #include <string>
+#include <iostream>
+#include <time.h>
+#include <vector>
+#include <iomanip>
+#include <CoreWindow.h>
 using namespace std;
+/*
+Разработать программу, которая удаляет в целочисленной матрице столбец с номером k.
+Размеры матрицы и значение k вводятся пользователем с клавиатуры. 
+Значения элементов матрицы формируются случайным образом в диапазоне [-99, 99], 
+матрица хранится в памяти в виде динамического массива.
+*/
 
-float minl(float a[],int k,int size){
-	if (k==size-1) return a[k];
-	float c = a[k];
-	float d = minl(a,k+1,size);
-	if(c<d) return c;
-	return d;
-}
-float min(float* a,int size){
-	return minl(a,0,size);
-}
 int _tmain(int argc, _TCHAR* argv[])
 {
-	int n;
-	system("chcp 1251");
-	system("cls");
-	cout<<"Введите количество элементов массива: ";
-	cin>>n;
-	cout<<"Введите сами элементы"<<endl;
-	//int arr[] = {1,2,6,-1,0,5};
-	int a = 1;
-	int b = 5;
-	float *x = new float[n];
-	string s;
-	float tmp;
-	for(int i=0;i<n;i++){
-		while(true){
-			cin>>s;
-      tmp=atoi(s.c_str());
-			if(!(tmp==0 && s!="0")){
-				x[i]=tmp;
-				break;
-			} else {
-				cout<<"Некорректный ввод"<<endl;
-				cout<<"Попробуйте еще раз"<<endl;
-			}
+	SetConsoleOutputCP(1251);
+    SetConsoleCP(1251);
+	string x_s,y_s,k_s;
+	int x,y,k,r;
+	srand(time(NULL));
+	bool running = true;
+	while(running){
+		cout<<"Введите размеры матрицы "<<endl;
+		cout<<"Введите количество строк:";
+		cin>>x_s;
+		cout<<"Введите количество столбцов:";
+		cin>>y_s;
+		cout<<"Введите k, номер столбца, который нужно удалить: ";
+		cin>>k_s;
+		x = atoi(x_s.c_str());
+		y = atoi(y_s.c_str());
+		k = atoi(k_s.c_str());
+		if( (x==0 && x_s[0]!='0') || (y==0 && y_s[0]!='0') || (k==0 && k_s[0]!='0') ){
+			cout<<"Некорректные данные"<<endl;
 		}
-	} 
+		else running = false;
+	}
+	int** a;
+	cout<<"Исходная матрица:"<<endl;
+	a = new int *[x];
+	for(int i=0;i<x;i++){
+		a[i]=new int[y];
+		for(int j=0;j<y;j++){
+			r=rand()%100;
+			r=rand()%2?r:-r;
+			a[i][j]=r;
+			cout<<setw(3)<<a[i][j]<<" ";
+		}
+		cout<<endl;
+	}
 	cout<<endl;
-	cout<<"Минимальный элемент: "<<min(x,n)<<endl;
+	cout<<"Итоговая матрица:"<<endl;
+	for(int i=0;i<x;i++){
+		for(int j=0;j<y-1;j++){
+			if (j>=k-1)
+				a[i][j]=a[i][j+1];
+			cout<<setw(3)<<a[i][j]<<" ";
+		}
+		cout<<endl;
+	}
 	_getch();
 	return 0;
 }
+
