a28b93c331f85c512514a26b20608df906ab1620
Marat Nagayev <nagaevmt49@gmail.com>
2021-06-02 18:29:49 +0300

    Update prog6.cpp

--- prog6.cpp
+++ prog6.cpp
@@ -5,8 +5,9 @@
 #include <conio.h>
 #include <iostream>
 #include <cstdlib>
+#include <fstream>
+#include <string>
 #include <vector>
-#include <cstdio>
 using namespace std;
 
 /*
@@ -15,62 +16,49 @@ using namespace std;
 а также их количество. 
 Имя читаемого файла и название буквы запрашиваются с клавиатуры.
 */
-int _tmain(int argc, _TCHAR* argv[])
+int _tmain()
 {
-	vector<char> tmp;
-	vector<vector<char>> words;
-	FILE *f;
+	vector<string> words;
 	int counter = 0;
-	char filename[255];
+	string filename;
 	system("chcp 1251");
 	system("cls");
 	//..\\Debug\\a.txt
-	while(1){
+	ifstream f;
+	while(!f.is_open()){
 		cout<<"Введите название файла ";
-		gets_s(filename,255);
-		int err = fopen_s(&f,filename, "r");
-		if(err!=0){
+		getline(cin,filename);
+		f.open(filename);
+		if(!f.is_open()){
 		  cout<<"Неправильное имя файла"<<endl;
 		  cout<<"Попробуйте еще раз"<<endl;
-		} else {
-			break;
 		}
 	}
-	cout<<"Введите букву:";
 	char c;
-	c=getchar();
-	fseek(f, 0, SEEK_END);
-	long fsize = ftell(f);
-	fseek(f, 0, SEEK_SET);  /* same as rewind(f); */
-
-	char *content = (char*) malloc(fsize + 1);
-	fread(content, sizeof(char), fsize, f);
-	fclose(f);
-	content[fsize] = 0;
-	int i,j;
-	for(i=0;i<fsize;i++){
-		char current = content[i];
-		if(current==' '){
-			words.push_back(tmp);
-			tmp.clear();
-		}
-		else tmp.push_back(current);
+	while(!isalpha(c)){
+	    cout<<"Введите букву:";
+	    cin>>c;
+	    if (!isalpha(c)) cout<<"Введена не буква!"<<endl;
 	}
-	cout<<"Слова: "<<endl;
-	if(tmp.size()!=0) words.push_back(tmp);
-	for(int i=0;i<words.size();i++){
-		if(words[i][0]==c){
-			for(int j=0;j<words[i].size();j++){
-				cout<<words[i][j];
-			}
-			counter++;
-			cout<<' ';
-		}
+    	string word;
+	while (f >> word){
+	    if (word[0]==c){
+	        counter++;
+	    } 
+	    words.push_back(word);
 	}
+	f.close();
 	if(counter==0){
 		cout<<"Слов нет";
 	} else {
-		cout<<endl<<"Количество: "<<counter;
+		cout<<endl<<"Количество: "<<counter<<endl;
+		cout<<"Слова: ";
+	}
+	for(int i=0;i<words.size();i++){
+		if(words[i][0]==c){
+			cout<<words[i];
+			cout<<' ';
+		}
 	}
 	_getch();
 	return 0;
